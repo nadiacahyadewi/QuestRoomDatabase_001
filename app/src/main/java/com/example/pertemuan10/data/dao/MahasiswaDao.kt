2df159df3d5e294d6bbf6898052ead6de0947f28
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-13 17:21:40 +0700

    getAllMahasiswa

--- app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
@@ -2,10 +2,17 @@ package com.example.pertemuan10.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insetMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
 }
+
