6c75f864ff421f5a839cc053dfcc8c706c458440
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-13 18:18:21 +0700

    menambahkan getAll, getMhs, deleteMhs, updateMhs

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -1,16 +1,14 @@
 package com.example.pertemuan10.repository
 
-import androidx.room.Query
 import com.example.pertemuan10.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
-    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
-    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 
-    fun getMhs(nim: String) : Flow<Mahasiswa>
+    fun getMhs(nim: String): Flow<Mahasiswa>
 
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
 
