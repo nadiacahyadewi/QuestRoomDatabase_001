ed13571c51a43c577e273d62c55adfa60cd91384
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-13 18:15:39 +0700

    update

--- app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan10.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -10,6 +12,18 @@ import kotlinx.coroutines.flow.Flow
 interface MahasiswaDao {
     @Insert
     suspend fun insetMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 
 }
 
