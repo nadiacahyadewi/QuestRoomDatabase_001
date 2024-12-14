efd1e8aca67c4d4ff5c5c74b40dcb83fc81abb36
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2024-12-14 10:37:41 +0700

    menambahkan DestinasiDetail, DestinasiHome, DestinasiUpdate pada AlamatNavigasi.kt untuk menandai view

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,22 @@ package com.example.pertemuan10.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+
+
+
